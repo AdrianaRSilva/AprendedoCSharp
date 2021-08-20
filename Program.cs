4cf1b3aec187cc222891fa5c53ccb2d61f92a04c
NinhaRilva <adrianarafael2010@gmail.com>
2021-08-20 08:39:36 -0300

    primeiro commite

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CursoCSharp.Fundamentos;
+
+namespace CursoCSharp {
+    class Program {
+        static void Main(string[] args) {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+                {"Comentários- Fundamentos", Comentarios.Executar},
+                {"Variaveis E Constantes -Fundamentos",VariaveisEConstantes.Executar},
+                {"Iferência -Fundamentos",Inferencia.Executar},
+                {"Interpolação -Fundamentos", Interpolacao.Executar},
+                {"Notação Ponto -Fundamentos", NotacaoPonto.Executar},
+                {"formantando Numeros -Fundamentos", FormatandoNumero.Executar},
+                {"Conversôs -Fundamentos", Conversoes.Executar},
+                {"Operadores Aritmeticos -Fundamentos", OperadoresAritmeticos.Executar},
+                 {"Operadores Relacionais -Fundamentos", OperadoresRealcionais.Executar},
+
+            });
+
+
+            central.SelecionarEExecutar();
+            
+        }
+    }
+}
