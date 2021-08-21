458bd6e904ebcbe8e80ec5ac4fe61a49f23274d4
NinhaRilva <adrianarafael2010@gmail.com>
2021-08-21 20:17:17 -0300

    finalizado aparte de  fundamentos

--- Fundamentos/OperadoresUnarios.cs
+++ Fundamentos/OperadoresUnarios.cs
@@ -17,15 +17,15 @@ namespace CursoCSharp.Fundamentos
             Console.WriteLine(-valorNegatio); // quando vc colocar uso sinal negativo na frente  do operando vc enverte o valor dele
             Console.WriteLine(!Booleano); // usando  !  vai ser invertido o valor  se for verdario passa para false
 
-            numero1++; // posso colocar  ++ na frete ou  atras 
+            numero1++; // pós fixado
             Console.WriteLine(numero1);
            
-            --numero1; // posso colocar  -- na frete ou  atras
+            //--numero1; // pré fixado
             Console.WriteLine(numero1);
            
             Console.WriteLine(numero1 == --numero2);
 
-            Console.WriteLine(numero1 +  " "+" " + numero2);
+            Console.WriteLine($"{numero1} {numero2}");
 
         }
     }
--- Program.cs
+++ Program.cs
@@ -8,18 +8,19 @@ namespace CursoCSharp {
         static void Main(string[] args) {
             var central = new CentralDeExercicios(new Dictionary<string, Action>() {
                 {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
-                {"Comentários- Fundamentos", Comentarios.Executar},
-                {"Variaveis E Constantes -Fundamentos",VariaveisEConstantes.Executar},
-                {"Iferência -Fundamentos",Inferencia.Executar},
-                {"Interpolação -Fundamentos", Interpolacao.Executar},
-                {"Notação Ponto -Fundamentos", NotacaoPonto.Executar},
-                {"formantando Numeros -Fundamentos", FormatandoNumero.Executar},
-                {"Conversôs -Fundamentos", Conversoes.Executar},
-                {"Operadores Aritmeticos -Fundamentos", OperadoresAritmeticos.Executar},
-                {"Operadores Relacionais -Fundamentos", OperadoresRealcionais.Executar},
-                {"Operadores Lógicos -Fundamentos", OperadoresLogicos.Executar},
-                {"Operadores de Atibuição -Fundamentos", OperadoresDeAtribuicao.Executar},
-                {"Operadores Unários -Fundamentos", OperadoresUnarios.Executar},
+                {"Comentários - Fundamentos", Comentarios.Executar},
+                {"Variaveis E Constantes - Fundamentos",VariaveisEConstantes.Executar},
+                {"Iferência - Fundamentos",Inferencia.Executar},
+                {"Interpolação - Fundamentos", Interpolacao.Executar},
+                {"Notação Ponto - Fundamentos", NotacaoPonto.Executar},
+                {"formantando Numeros - Fundamentos", FormatandoNumero.Executar},
+                {"Conversôs - Fundamentos", Conversoes.Executar},
+                {"Operadores Aritmeticos - Fundamentos", OperadoresAritmeticos.Executar},
+                {"Operadores Relacionais - Fundamentos", OperadoresRealcionais.Executar},
+                {"Operadores Lógicos - Fundamentos", OperadoresLogicos.Executar},
+                {"Operadores de Atibuição - Fundamentos", OperadoresDeAtribuicao.Executar},
+                {"Operadores Unários - Fundamentos", OperadoresUnarios.Executar},
+                {"Operadores Ternário - Fundamentos", OperadorTernario.Executar},
 
             });
 
